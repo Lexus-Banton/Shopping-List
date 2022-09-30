b4d62e5a09fb53e5f2090a3f693f0fed76e7bead
Lexus Banton <lexusbanton.kr@gmail.com>
2022-09-30 00:53:10 -0700

    create lists

--- app.js
+++ app.js
@@ -4,9 +4,31 @@ import './auth/user.js';
 import { createLists } from './fetch-utils.js';
 
 /* Get DOM Elements */
+const addListForm = document.getElementById('add-list-form');
+const removeButton = document.getElementById('remove-button');
+const errorDisplay = document.getElementById('');
 
 /* State */
+let lists = [];
+let error = null;
 
 /* Events */
+addListForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(addListForm);
+    const newList = { description: formData.get('description') };
+
+    const response = await createLists(newList);
+    error = response.error;
+    const list = response.data;
+    console.log(list);
+    if (error) {
+        displayError();
+    } else {
+        lists.push(list);
+        displayLists();
+        addListForm.reset();
+    }
+});
 
 /* Display Functions */
